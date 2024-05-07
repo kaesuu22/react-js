dca9c487cdcf9703747b3c9e5e91e4cb0c40ef3d
kaesuu22 <bajeniok@gmail.com>
2024-05-07 22:15:29 +0800

    project

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import Students from "./pages/Students";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1 className='text-black text-3xl text-center justify-center '>hi im kahtyy</h1>
-    </>
-  )
-}
+    <Router>
+      <div className="">
+      <div className="flex flex-col h-screen w-screen">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/admin" element={<Admin />} />
+            <Route exact path="/students" element={<Students />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ my-project/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+        <footer className="bg-blue-400 text-center justify-center text-white py-4">
+            <div className="container mx-auto px-5">
+            <p>&copy; @2024 Footer</p>
+            </div>
+        </footer>
+    )
+  }
+}
+
+export default Footer
